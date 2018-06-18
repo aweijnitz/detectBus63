4868b727a46913a1acf199105627200367ecfbd4
anders <andersw@noone.it>
2018-06-18 22:23:35 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+var http = require('http');
+const cv = require('opencv4nodejs');
+const mjpegServer = require('mjpeg-server');
+
+const PORT = process.env.PORT || 8081;
+const DEVICE = process.env.DEVICE || 0;
+
+// open capture from webcam
+const wCap = new cv.VideoCapture(DEVICE);
+
+
+http.createServer(function (req, res) {
+    let mjpegReqHandler = mjpegServer.createReqHandler(req, res);
+
+    // Return video stream
+    if (/stream/.test(req.url)) {
+        for (let i = 0; i < 100; i++) {
+            wCap.readAsync((err, frame) => {
+                let jpgBuf = cv.imencode('.JPEG', frame);
+                mjpegReqHandler.write(jpgBuf);
+            });
+        }
+
+    } else if (/frame/.test(req.url)) {
+        // A request to http://localhost/frame returns a single frame as a jpeg
+
+        //res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        //res.end(camera.frame);
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(fs.readFileSync('./client/index.html'));
+    }
+
+}).listen(PORT);
